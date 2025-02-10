4aad9623a4b3f85847c8972f7f4d7e00daab436d
elisabethwfm <elisabeth.wolfram@gmail.com>
2025-02-10 14:48:22 +0100

    deleted console.log

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ function displayPoem(response) {
     delay: 50,
     Cursor: "",
   });
-
-  console.log("poem generated");
 }
 
 function generatePoem(event) {
@@ -16,12 +14,8 @@ function generatePoem(event) {
   let apiKey = "9of48ba08265c3b61efaffet394bd310";
   let prompt = `user instructions: Generate a german poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic poem expert and love to write short poem. Your mission is to generate a 4 line poem and separate each line with <br/><br/> and add a <br/> at the begging as well. Make sure to follow the user instructions. Sign the Poem with `~ SheCodes AI` inside a <strong> element.";
+    "You are a romantic poem expert and love to write short poem. Your mission is to generate a 4 line poem and separate each line with <br/><br/> and add a <br/> at the begging as well. Make sure to follow the user instructions. Sign the Poem with `<br/>~ SheCodes AI` inside a <strong> element.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  console.log("generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
 
   axios.get(apiUrl).then(displayPoem);
 }
